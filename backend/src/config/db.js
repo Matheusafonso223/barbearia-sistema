f0b7849020f312e23cc3f4e0ac1308cd1e6fb6a9
matheusafonso223 <matheusnegocios2223@gmail.com>
2026-04-25 13:10:36 -0300

    Finalizando ajustes do sistema de barbearia

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,24 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
+let db;
+
+if (process.env.DATABASE_URL) {
+  db = mysql.createConnection(process.env.DATABASE_URL);
+} else {
+  db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+  });
+}
 
 db.connect((erro) => {
   if (erro) {
     console.error("Erro ao conectar no MySQL:", erro);
   } else {
-    console.log("Conectado ao MySQL com sucesso 🚀");
+    console.log("Conectado ao MySQL com sucesso");
   }
 });
 
